549b0b8711d6b98f11fd62c2e00d9fa954918434
hedianjie <1163486362@qq.com>
2019-07-18 16:52:56 +0800

    s

---
+++ pages/createAddress/index.vue
@@ -0,0 +1,33 @@
+<template>
+	<view class="content">
+		
+	</view>
+</template>
+
+<script>
+	import faIcon from "../../components/kilvn-fa-icon/fa-icon.vue";
+	export default {
+		onLoad(option) {
+			// 通过路由参数决定导航栏文字 编辑或者新建
+			uni.setNavigationBarTitle({
+				title: "添加地址"
+			})
+		},
+		
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	page{
+		background: #F8F9FA;
+	}
+
+</style>
